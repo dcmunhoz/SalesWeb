db56b1cfb5dc454b61d4bdcfdfeca3f98bdb4da2
dcmunhoz <dc.munhoz@hotmail.com>
2021-07-10 15:13:01 -0300

    CRUD Scaffolding

--- SalesWeb/Controllers/HomeController.cs
+++ SalesWeb/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SalesWeb.Models;
-using SalesWeb.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
